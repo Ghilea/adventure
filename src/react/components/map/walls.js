54b76585a578a3516d00cc8db125e00f9c594653
Dennis Karlsson <tougent@gmail.com>
2022-05-12 15:53:04 +0200

    update

--- src/react/components/map/walls.js
+++ src/react/components/map/walls.js
@@ -1,8 +1,11 @@
 import React, {useEffect, useRef, useState} from 'react';
 import { useBox } from '@react-three/cannon';
 import * as texture from './buildLevel/textures';
+import { Color } from 'react-three-fiber'
 
 export const Walls = ({rotation, position, type, ...props}) => {
+    const [hover, setHover] = useState(null);
+
     const [ref] = useBox(() => ({
         args: [8, 4, 0.5],
         rotation,
@@ -10,23 +13,26 @@ export const Walls = ({rotation, position, type, ...props}) => {
         ...props
     }))
 
-    console.log(texture.stone())
-    const side =
-        [...Array(6)].map((_, index) => ( 
-        <meshStandardMaterial attachArray = 'material'
-            map = {
-                (type == 'stone') ? texture.stone() : console.log('inget')
-            }
-            key = {
-                index
-            }
-            />
-        ))
-    
     return (
-        <mesh ref = {ref} castShadow>
+        <mesh ref = {ref} castShadow 
+            onPointerMove={(event) => {
+                event.stopPropagation();
+                setHover(Math.floor(event.faceIndex / 2));
+            }}>
+            {[...Array(6)].map((_, index) => ( 
+            <meshStandardMaterial attachArray = 'material'
+                map = {
+                    (type == 'stone') ? texture.stone() : console.log('inget')
+                }
+                key = {
+                    index
+                }
+                color={
+                    (hover === index) ? 'grey' + console.log('test') : 'white'
+                }
+                />
+            ))}
             <boxGeometry args = {[8, 4, 0.5]} />
-            {side}
         </mesh>
     )
 
