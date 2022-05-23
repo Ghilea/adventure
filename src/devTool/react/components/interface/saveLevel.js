3749aaa8c3b6be9273a2be470d53375ef2fc0831
Dennis Karlsson <tougent@gmail.com>
2022-05-23 15:33:14 +0200

    update

--- src/devTool/react/components/interface/saveLevel.js
+++ src/devTool/react/components/interface/saveLevel.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import { build } from '../store';
+import { build, player } from '../store';
 import { Create } from '../../../../shared/components/Crud';
 
 export const SaveLevel = () => {
     
     const store = build(state => state);
+    const storePlayer = player(state => state);
     const saveLevel = build(state => state.saveLevel);
 
     const handleSave = () => {
-        saveLevel(store.walls)
+        saveLevel({'walls': store.walls, 'player': storePlayer.playerMark})
         
-        const url = `http://localhost:3000/createLevel`;
+        /*const url = `http://localhost:3000/createLevel`;
 
         Create(url, {
             content: JSON.stringify(store.walls)
-        });
+        });*/
     }
 
     return (
