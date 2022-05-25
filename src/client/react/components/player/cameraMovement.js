c9b951326e35b096704967b6105897b9c2c9505a
Dennis Karlsson <tougent@gmail.com>
2022-05-25 13:51:50 +0200

    update

--- src/client/react/components/player/cameraMovement.js
+++ src/client/react/components/player/cameraMovement.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { PointerLockControls as PointerLockControlsImpl } from '@react-three/drei';
 import { extend, useThree } from '@react-three/fiber';
-import { map } from '@comp/store';
+import { map, player, enemy, combat } from '@comp/store';
 
 extend({ PointerLockControlsImpl });
 
 export const CameraMovement = () => {
 
+    const storePlayer = player(state => state);
+    const storeEnemy = enemy(state => state);
+    const storeCombat = combat(state => state);
     const storeMap = map(state => state);
 
     const [unlocked, setUnlocked] = useState(false);
@@ -23,6 +26,46 @@ export const CameraMovement = () => {
             controls.current.lock();
         }
     }, [storeMap.camera])
+
+    useEffect(() => {
+        if (!storeMap.camera && !unlocked) {
+            document.addEventListener('click', (event) => {
+                handleMouseClick(event)
+            })
+        }
+    }, [])
+
+    const handleMouseClick = (event) => {
+        console.log('click');
+        event.preventDefault();
+
+        if (event.type === 'click' && storeEnemy.hp > 0 && storePlayer.canAttack) {
+            console.log('left');
+
+            const playerText = createElement(
+                'p', {
+                    key: 'combatScrollPlayer',
+                    className: 'combatScrollPlayer combatScrollAnimation'
+                },
+                storePlayer.dps
+            )
+
+            storePlayer.allowAttack(false, true);
+            storeEnemy.gettingHit(false, (storeEnemy.hp -= storePlayer.dps));
+            storeCombat.changeText(playerText);
+
+            setTimeout(() => {
+                storePlayer.allowAttack(true, false);
+                storeCombat.changeText(null);
+            }, 1500)
+
+        } else if (event.type === 'mousedown' && event.button === 2) {
+            storePlayer.isBlock(true);
+        } else if (event.type === 'contextmenu') {
+            storePlayer.isBlock(false);
+        }
+
+    }
 
     return (
             <>
