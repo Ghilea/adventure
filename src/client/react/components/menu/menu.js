d751153bd6a2161f8811f6a5337e7d6fd3ea8d9a
Dennis <tougent@gmail.com>
2022-05-27 06:40:54 +0200

    update

--- src/client/react/components/menu/menu.js
+++ src/client/react/components/menu/menu.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { menu } from '@comp/store';
 import CreateWindow from '@comp/menu/createProtagonist';
-import { CharacterList } from './characterList';
+import { CharacterList } from '@comp/menu/characterList';
+import { Options } from '@comp/menu/options'
 
 export const Menu = () => {
 
