cc0cc53ddc4189050901271e3f0a7e0184ab0e65
Dennis Karlsson <tougent@gmail.com>
2022-05-30 13:17:49 +0200

    update

--- src/client/react/components/menu/login.js
+++ src/client/react/components/menu/login.js
@@ -43,7 +43,7 @@ const Login = () => {
             setTimeout(() => {
                 stop();
                 setPlaying(false);
-            }, 10);
+            }, 1000);
         }
     },[storeMenu.login])
 
