ec416fcbf388c2f0aa82ee7cd68586929d341320
Dennis Karlsson <tougent@gmail.com>
2022-05-31 10:35:54 +0200

    update

--- src/shared/components/models/torch.js
+++ src/shared/components/models/torch.js
@@ -1,22 +1,50 @@
 import React, { useEffect, useRef, useState } from "react";
+import { useLoader } from '@react-three/fiber';
 import { useGLTF } from "@react-three/drei";
+import { TextureLoader, RepeatWrapping } from 'three';
 import { Flame } from '@shared/components/models/flame';
+import t from '@shared/assets/images/3d/torch/texture.jpg';
 
 export const Torch = (props) => {
     
     const group = useRef();
-    const {nodes, materials} = useGLTF("./assets/images/3d/Torch.gltf");
+    const {nodes, materials} = useGLTF("./assets/images/3d/torch/Torch.gltf");
+
+    const [texture] = useLoader(TextureLoader, [t]);
+    texture.wrapS = RepeatWrapping;
+    texture.wrapT = RepeatWrapping;
+    texture.repeat.set(10, 10);
         
     return ( 
         <group castShadow receiveShadow ref = {group} {...props} dispose = {null} >
             
-            <mesh castShadow receiveShadow geometry = {nodes.lsdGroup15825.geometry} material = {nodes.lsdGroup15825.material}/> 
+            <mesh castShadow receiveShadow geometry = {
+                nodes.lsdGroup15825.geometry
+            }
+            material = {
+                nodes.lsdGroup15825.material
+            }
+            material-color = {
+                'red'
+            } >
+                <meshStandardMaterial attach="material" 
+                map={texture}
+                transparent={true} />
+            </mesh> 
             
-            <mesh castShadow receiveShadow geometry = {nodes.chorchGroup1799_Mashal.geometry} material = {nodes.chorchGroup1799_Mashal.material}/> 
+            <mesh castShadow receiveShadow geometry = {nodes.chorchGroup1799_Mashal.geometry} material = {nodes.chorchGroup1799_Mashal.material} >
+                <meshStandardMaterial attach="material" 
+                map={texture}
+                transparent={true} />
+            </mesh>  
         
-            <mesh castShadow receiveShadow geometry = {nodes.Mashal_MMMGroup63307.geometry} material = {nodes.Mashal_MMMGroup63307.material}/>
+            <mesh castShadow receiveShadow geometry = {nodes.Mashal_MMMGroup63307.geometry} material = {nodes.Mashal_MMMGroup63307.material} >
+                <meshStandardMaterial attach="material" 
+                map={texture}
+                transparent={true} />
+            </mesh> 
 
-            <Flame position={[-2, 0.5, 0]}/>
+            <Flame position={[-5, 0.5, 0]}/>
 
         </group>
     );
