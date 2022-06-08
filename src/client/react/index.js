9fd0107dfadc245afaa560f89c55ce6cdb086de0
Dennis Karlsson <tougent@gmail.com>
2022-06-08 12:09:51 +0200

    update

--- src/client/react/index.js
+++ src/client/react/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { MapEditor } from '@devComp/map/editor';
 import { App } from '@comp/app';
 
 const container = document.querySelector('main');
 const root = createRoot(container);
 
-root.render(<App />);
+root.render(<MapEditor />);
