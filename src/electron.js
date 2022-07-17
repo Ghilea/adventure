0481bc808e415a01ae6b74b8a8792f05331b33fc
Dennis Karlsson <tougent@gmail.com>
2022-07-17 18:43:36 +0200

    electron

--- src/electron.js
+++ src/electron.js
@@ -8,6 +8,8 @@ const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
+        fullscreen: false,
+        fullscreenable: true,
         webPreferences: {
             nodeIntegration: true,
             enableRemoteModule: true
