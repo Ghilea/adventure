fe73e4e39ba45822e4977a1c4d1286280c252916
Dennis Karlsson <tougent@gmail.com>
2022-11-10 20:21:51 +0100

    update

--- src/assets/models/objects/walls/walls.jsx
+++ src/assets/models/objects/walls/walls.jsx
@@ -1,20 +1,31 @@
-import React from "react";
+import { useEffect } from "react";
 import { useGLTF } from "@react-three/drei";
 import { Select } from '@react-three/postprocessing';
 import { useSelectObject } from '@editor/hooks/use-select-object';
-import asset from './wall.glb';
 import UseSolid from "../../components/use-solid";
+import useViewportScale from "../../components/use-viewport-scale";
+import asset from './wall.glb';
 
 export const Wall_1 = (props) => {
   const { nodes, materials  } = useGLTF(asset);
 
   const [isSelected, handleClick] = useSelectObject();
 
+  const [scaleIt, setViewportScale] = useViewportScale();
+
+  useEffect(() => {
+    setViewportScale({
+      width: 1.6,
+      height: 1.5,
+      depth: 1.6
+    })
+  }, [])
+
   return (
     <group dispose = {null} 
       onClick={handleClick}
     {...props} 
-    scale = {[1.6, 1.5, 1.6]}
+    scale = {scaleIt}
     >
       <Select enabled={isSelected}>
       <mesh 
